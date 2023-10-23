6a074c742500a05bce845d24d6d3207b0ffa3f4b
Shlomo Kalinsky <93456838+sdkkds2125@users.noreply.github.com>
2023-10-23 19:14:12 -0400

    Add project files.

---
+++ HeapSortLL.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapsortHW
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node() { }  
+        public Node(int data) 
+        { 
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+    internal class HeapSortLL
+    {
+        private Node current;
+        private Node tempHighestForHeapify;
+        private int[] nums = new int[20];
+        private Random rnd = new Random();
+        private int depth;
+        public void sortAsLL(Node head) 
+        {
+            depth = maxDepth(head);
+            for (int i = depth;i > 0;i--) 
+            {
+                checkOfNodesAtXDepth(head,0,i);
+            }
+            while (head.Right != null && head.Left != null) 
+            {
+                depth = maxDepth(head);
+                Node temp = getAndRemoveNodeAtXDepth(head, 0, depth);
+                //swap(head,)
+            } 
+        }
+
+
+        public Node generateNumsAndTreeifyThem() {
+            for (int i = 0; i < 20; i++) { 
+                 nums[i] = rnd.Next(100);            
+            }
+            Console.WriteLine("the list before sorting");
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                Console.Write(nums[i] + " - ");
+            }
+            Console.WriteLine("\n");
+            Node head = makeNumsIntoTree(nums,0);
+            return head;
+        }
+
+        public Node makeNumsIntoTree(int[] arr, int i)
+        {
+            Node root = null;
+            // Base case for recursion
+            if (i < arr.Length)
+            {
+                root = new Node(arr[i]);
+
+                // insert left child
+                root.Left = makeNumsIntoTree(arr, 2 * i + 1);
+
+                // insert right child
+                root.Right = makeNumsIntoTree(arr, 2 * i + 2);
+            }
+            return root;
+        }
+
+        int maxDepth(Node node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                /* compute the depth of each subtree */
+                int lDepth = maxDepth(node.Left);
+                int rDepth = maxDepth(node.Right);
+
+                /* use the larger one */
+                if (lDepth > rDepth)
+                    return (lDepth + 1);
+                else
+                    return (rDepth + 1);
+            }
+        }
+      
+        void checkOfNodesAtXDepth(Node root, int currDepth, int wantedDepth)
+        {
+            //just in case the level wanted doesn't exist
+            if (root == null)
+                return;
+
+            // If the wanted level and current level 
+            // are same check if node there is heapified
+            if (currDepth == wantedDepth)
+            {
+                heapifyForLL(root);
+            } //if greater then done cut it off there like a break
+            else if (currDepth > wantedDepth) 
+            {
+                return;
+            }
+
+            // keep going down the left and right subtrees 
+            checkOfNodesAtXDepth(root.Left, currDepth + 1,wantedDepth);
+            checkOfNodesAtXDepth(root.Right, currDepth + 1, wantedDepth);
+        }
+
+
+        private int getAndRemoveNodeAtXDepth(Node root, int currDepth, int wantedDepth)
+        {
+            int data;
+            if (root == null)
+            {
+                return -int.MaxValue;
+            }
+            // If the wanted level and current level 
+            // are same delete that node and return its data in a new one
+            else if (currDepth == wantedDepth)
+            {
+                data = root.Data;
+                root = null;
+                return data;
+            }
+            
+            else
+            {
+                // keep going down the left and right subtrees 
+                getAndRemoveNodeAtXDepth(root.Left, currDepth + 1, wantedDepth);
+                getAndRemoveNodeAtXDepth(root.Right, currDepth + 1, wantedDepth);
+                return data + 0;
+            }
+        }
+
+        private void heapifyForLL(Node root)
+        {
+            tempHighestForHeapify = root;
+
+            if (root != null)
+            {
+                if (root.Left != null)
+                {
+                    if (root.Left.Data > tempHighestForHeapify.Data)
+                    {
+                        tempHighestForHeapify = root.Left;
+                    }
+                }
+                if (root.Right != null)  
+                {
+                    if(root.Right.Data > tempHighestForHeapify.Data) 
+                    {
+                        tempHighestForHeapify = root.Right;
+                    }
+                }
+                if(tempHighestForHeapify.Data != root.Data)
+                {
+                    swap(root, tempHighestForHeapify);
+                    heapifyForLL(tempHighestForHeapify);
+                }
+
+            }
+        }
+
+        private void swap(Node root, Node tempHighestForHeapify)
+        {
+            int temp = root.Data;
+            root.Data = tempHighestForHeapify.Data;
+            tempHighestForHeapify.Data = temp;
+
+        }
+
+        public void inOrderPrint(Node root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.Data + " ");
+                inOrderPrint(root.Left);
+                inOrderPrint(root.Right);
+            }
+        }
+
+   
+        public void test()
+        {
+            Node t2 = new Node();
+            int[] arr = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
+            t2 = makeNumsIntoTree(arr, 0);
+            Console.WriteLine(arr.Length);
+            Console.WriteLine(maxDepth(t2));
+            inOrderPrint(t2);
+            Node t3 = new Node(7);
+            Node temp = t3;
+            temp.Data = 99999999;
+            Console.WriteLine(temp.Data);
+            Console.WriteLine(t3.Data);
+            Console.WriteLine();
+            Console.WriteLine();
+            Node testHead = new Node(5);
+            testHead.Right = new Node(38);
+            Node test2 = new Node(46);
+            test2.Left = new Node(1);
+            test2.Left.Right = new Node(40);
+            testHead.Right.Right = new Node(2);
+            testHead.Right.Left = new Node(4);
+            testHead.Right.Left.Right = new Node(5);
+            int depthy = maxDepth(testHead);
+            Console.WriteLine("depthy " + depthy);
+            //swap(testHead, test2);
+            heapifyForLL(testHead);
+            inOrderPrint(testHead);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            heapifyForLL(test2);
+            inOrderPrint(test2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,245 @@
+﻿
+
+using HeapsortHW;
+
+public class HeapSortArr
+{
+    public static void sortAsArray(int[] arr)
+    {
+        int length = arr.Length;
+
+        // Build max heap 
+        for (int i = length / 2 - 1; i >= 0; i--)
+        {
+            heapifyAsArray(arr, length, i);
+        }
+        // One by one extract an element from heap
+        for (int i = length - 1; i > 0; i--)
+        {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapifyAsArray(arr, i, 0);
+        }
+    }
+
+    
+    static void heapifyAsArray(int[] arr, int length, int i)
+    {
+        int largest = i; // root
+        int leftChild = 2 * i + 1; // left 
+        int rightChild = 2 * i + 2; // right
+
+        // If left child is larger than root
+        if (leftChild < length && arr[leftChild] > arr[largest])
+            largest = leftChild;
+
+        // If right child is larger than largest so far (either root or left)
+        if (rightChild < length && arr[rightChild] > arr[largest])
+            largest = rightChild;
+
+        // If largest was changed so is no longer ponting to the root
+        if (largest != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            // Recursively heapify the affected sub-tree
+            //since now I have done a switch make sure that the rest is still hepified all the way down
+            heapifyAsArray(arr, length, largest);
+        }
+    }
+
+    // A utility function to print array
+    public static void printArray(int[] arr)
+    {
+
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+
+
+}
+
+
+
+
+class Program
+{
+    static int Nodes;
+    static Node First, Current;
+
+    public static void Run2()
+    {
+        int i, j;
+        int[] dataList = { 3, 5, 67, 43, 52, 11, 23, 4, 8, 54, 32, 76, 39, 23 };
+
+        foreach (int num in dataList)
+        {
+
+            Current = new Node();
+            Current.Data = num;
+            Nodes++;
+           
+            //
+            for (i = Nodes, j = -1; i > 0; i >>= 1, j++) ;
+
+            if (First == null)
+            {
+                First = Current;
+                First.Left = null;
+                First.Right = null;
+            }
+            else
+                AddNode(First, First, j - 1);
+
+        }
+
+
+
+        while (Nodes > 0)
+        {
+            Console.Write($"{First.Data} -> ");
+            for (i = Nodes, j = -1; i > 0; i >>= 1, j++) ;
+            GetRoot(First, j - 1);
+            Nodes--;
+            HeapSort(First);
+        }
+
+        Console.WriteLine("\n\n");
+    }
+
+    static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void AddNode(Node tmp1, Node parent, int pos)
+    {
+        int dirxn = GetBit(Nodes, pos);
+
+        if (pos == 0)
+        {
+            if (dirxn == 1)
+                tmp1.Right = Current;
+            else
+                tmp1.Left = Current;
+
+            Current.Left = null;
+            Current.Right = null;
+
+            if (Current.Data > tmp1.Data)
+                Swap(ref Current.Data, ref tmp1.Data);
+        }
+        else
+        {
+            if (dirxn == 1)
+                AddNode(tmp1.Right, tmp1, pos - 1);
+            else
+                AddNode(tmp1.Left, tmp1, pos - 1);
+        }
+
+        if (tmp1.Data > parent.Data)
+            Swap(ref parent.Data, ref tmp1.Data);
+    }
+
+    static void GetRoot(Node tmp, int pos)
+    {
+        int dirxn;
+
+        if (Nodes == 1)
+            return;
+
+        while (pos > 0)
+        {
+            dirxn = GetBit(Nodes, pos);
+
+            if (dirxn == 1)
+                tmp = tmp.Right;
+            else
+                tmp = tmp.Left;
+
+            pos--;
+        }
+
+        dirxn = GetBit(Nodes, pos);
+
+        if (dirxn == 1)
+        {
+            First.Data = tmp.Right.Data;
+            tmp.Right = null;
+        }
+        else
+        {
+            First.Data = tmp.Left.Data;
+            tmp.Left = null;
+        }
+    }
+
+    static void HeapSort(Node tmp)
+    {
+        if (tmp.Right == null && tmp.Left == null)
+            return;
+
+        if (tmp.Right == null)
+        {
+            if (tmp.Left.Data > tmp.Data)
+                Swap(ref tmp.Left.Data, ref tmp.Data);
+        }
+        else
+        {
+            if (tmp.Right.Data > tmp.Left.Data)
+            {
+                if (tmp.Right.Data > tmp.Data)
+                {
+                    Swap(ref tmp.Right.Data, ref tmp.Data);
+                    HeapSort(tmp.Right);
+                }
+            }
+            else
+            {
+                if (tmp.Left.Data > tmp.Data)
+                {
+                    Swap(ref tmp.Left.Data, ref tmp.Data);
+                    HeapSort(tmp.Left);
+                }
+            }
+        }
+    }
+
+    static int GetBit(int num, int pos)
+    {
+        return (num & (1 << pos)) == 0 ? 0 : 1;
+    }
+}
+
+
+
+namespace yy {
+    class fert2 {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 4, 10,46,0,67, 3,6,89,4,6,98,65,23,12,28,45 };
+            Console.WriteLine("Original array:");
+            HeapSortArr.printArray(arr);
+
+            HeapSortArr.sortAsArray(arr);
+
+            Console.WriteLine("\nSorted");
+            HeapSortArr.printArray(arr);
+            Console.WriteLine("\n\n");
+            //Program.Run2();
+            HeapSortLL heapSortLL = new HeapSortLL();
+            heapSortLL.generateNumsAndTreeifyThem();
+            heapSortLL.test();
+        }
+    }
+}
